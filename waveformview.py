4f834a2907649c746452094546dd5421c2922239
zyads <zyads@umich.edu>
2022-07-12 00:12:44 -0400

    Data and meh code

---
+++ waveformview.py
@@ -0,0 +1,40 @@
+#make waveform viewer given bidmc##.dat file
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+# import matplotlib.ticker as ticker
+# import matplotlib.dates as mdates
+# import matplotlib.cm as cm
+# import matplotlib.colors as colors
+# import matplotlib.gridspec as gridspec
+# import matplotlib.patches as patches
+# import matplotlib.lines as mlines
+# import matplotlib.text as mtext
+# import matplotlib.image as mimage
+# import matplotlib.pyplot as plt
+# import matplotlib.widgets as mwidgets
+# import matplotlib.transforms as mtransforms
+# import matplotlib.artist as martist
+# import matplotlib.ticker as mticker
+# import matplotlib.axis as maxis
+# import matplotlib.spines as mspines
+# import matplotlib.font_manager as mfontmgr
+# import matplotlib.backends.backend_agg as mbackend_agg
+# import matplotlib.backends.backend_pdf as mbackend_pdf
+
+
+from IPython.display import display
+import matplotlib.pyplot as plt
+%matplotlib inline
+import numpy as np
+import os
+import shutil
+import posixpath
+
+import wfdb
+
+
+
+
